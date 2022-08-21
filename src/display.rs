ad4e8760eb779869c72bd50d81880aa4fc9e262e
Trevor Arjeski <tmarjeski@gmail.com>
2022-08-21 17:55:01 +0300

    Move display function to separate file

---
+++ src/display.rs
@@ -0,0 +1,90 @@
+use std::fmt::Write;
+
+use crate::args::{Book as ArgBook, Chapter, Verses};
+use crate::{Error, BOOKS};
+
+impl TryFrom<ArgBook> for String {
+    type Error = Error;
+
+    fn try_from(arg_book: ArgBook) -> Result<Self, Self::Error> {
+        let (alias, num) = arg_book.alias_num();
+        let book = BOOKS[num];
+
+        let mut out = String::new();
+        if let Some(ch) = arg_book.chapter() {
+            match ch {
+                Chapter::Single {
+                    chapter,
+                    verses: Some(verses),
+                } => {
+                    // print verses of a chapter
+                    let check_verse = |num: usize| {
+                        let verses = book[*chapter].len();
+                        if num > verses {
+                            Err(Error::InvalidVerse {
+                                num,
+                                title: book.title.to_string(),
+                                chapter: *chapter,
+                                verses,
+                            })
+                        } else {
+                            Ok(())
+                        }
+                    };
+                    match verses {
+                        Verses::Single(verse) => {
+                            check_verse(*verse)?;
+                            write_verse(&mut out, alias, *chapter, *verse, book[*chapter][*verse])?;
+                        }
+                        Verses::Range(range) => {
+                            for v in book[*chapter][range.clone()].iter().enumerate() {
+                                write_verse(&mut out, alias, *chapter, v.0 + range.start(), v.1)?;
+                            }
+                        }
+                    }
+                }
+                Chapter::Single { chapter, .. } => {
+                    // print a single chapter
+                    for v in book[*chapter].iter().enumerate() {
+                        write_verse(&mut out, alias, *chapter, v.0, v.1)?;
+                    }
+                }
+                Chapter::Range(range) => {
+                    // print a range of chapters
+                    for ch in book[range.clone()].iter().enumerate() {
+                        for v in ch.1.iter().enumerate() {
+                            write_verse(&mut out, alias, ch.0 + range.start(), v.0, v.1)?;
+                        }
+                    }
+                }
+            }
+        } else {
+            // print entire book!
+            for ch in book.chapters.iter().enumerate() {
+                for v in ch.1.iter().enumerate() {
+                    write_verse(&mut out, alias, ch.0, v.0, v.1)?;
+                }
+            }
+        }
+
+        Ok(out)
+    }
+}
+
+fn write_verse(
+    out: &mut String,
+    book: &str,
+    chapter_num: usize,
+    verse_num: usize,
+    verse: &str,
+) -> Result<(), std::fmt::Error> {
+    writeln!(
+        out,
+        "[{} {}:{}] {}",
+        book,
+        chapter_num + 1,
+        verse_num + 1,
+        verse
+    )?;
+    Ok(())
+}
